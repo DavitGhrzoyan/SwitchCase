476e1aec2af45c713168243c4affb920f453ec72
dghrzoyan.WorkPC <ghrzoyandavit@gmail.com>
2022-11-25 18:32:53 +0400

    Add project files.

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int orer = int.Parse(Console.ReadLine());
+            switch (orer)
+            {
+                case 1:
+                    Console.WriteLine("Erkushabti");
+                    break;
+                case 2:
+                    Console.WriteLine("Ereqshabti");
+                    break;
+                case 3:
+                    Console.WriteLine("4shabti");
+                    break;
+                case 4:
+                    Console.WriteLine("5shabti");
+                    break;
+                case 5:
+                    Console.WriteLine("Urbat");
+                    break;
+                case 6:
+                    Console.WriteLine("Shabat");
+                    break;
+                case 7:
+                    Console.WriteLine("Kiraki");
+                    break;
+                default:
+                    Console.WriteLine("Goyutyun chuni");
+                    break;
+                    
+            }
+
+        }
+    }
+}
